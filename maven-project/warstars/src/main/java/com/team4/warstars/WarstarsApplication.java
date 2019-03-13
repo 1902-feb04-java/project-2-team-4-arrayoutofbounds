9c83145f6867203fab290b045b0aabbc2d3a669e
Brandon Lee <tubocass@gmail.com>
2019-03-13 10:33:16 -0500

    Spring project created with JPA and Rest packages

---
+++ maven-project/warstars/src/main/java/com/team4/warstars/WarstarsApplication.java
@@ -0,0 +1,13 @@
+package com.team4.warstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarstarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarstarsApplication.class, args);
+	}
+
+}
