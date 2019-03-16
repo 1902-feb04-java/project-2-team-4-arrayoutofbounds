6d82828df4eac5b623500933db61dde99c9ab5b3
Brandon Lee <tubocass@gmail.com>
2019-03-16 11:44:59 -0500

    <Angular> Basic routing in place

--- SW-AOoB/src/app/app-routing.module.ts
+++ SW-AOoB/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-//   {path:'home', component: HomeComponent},
-//   {path:'catalogue', component: CatalogueComponent},
-//   {path:'inventory', component: InventoryComponent},
-//   {path:'history', component: HistoryComponent},
-//   {path:'locations', component: LocationsComponent},
-// ];
+import { HomepageComponent } from './homepage/homepage.component';
+import {OfficersComponent} from './officers/officers.component';
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path:'home', component: HomepageComponent},
+  {path:'officers', component: OfficersComponent}
+  // {path:'catalogue', component: CatalogueComponent},
+  // {path:'inventory', component: InventoryComponent},
+  // {path:'history', component: HistoryComponent},
+  // {path:'locations', component: LocationsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
