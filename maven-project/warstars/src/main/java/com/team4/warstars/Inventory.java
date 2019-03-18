5e7b6e6ab7bcb0a3d41ecf060c61aadcc9f3272d
Rodolfo <rudymartinez70@gmail.com>
2019-03-18 16:28:12 -0500

    Basic inventory class and interface based on officers

---
+++ maven-project/warstars/src/main/java/com/team4/warstars/Inventory.java
@@ -0,0 +1,93 @@
+package com.team4.warstars;
+
+import java.util.Arrays;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "inventories")
+public class Inventory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String location_id;
+	private String personal[];
+	private String vehicles[];
+	private String weapons[];
+	private int ammunition;
+
+	@Override
+	public String toString() {
+		return "Inventory [id=" + id + ", location_id=" + location_id + ", personal=" + Arrays.toString(personal)
+				+ ", vehicles=" + Arrays.toString(vehicles) + ", weapons=" + Arrays.toString(weapons) + ", ammunition="
+				+ ammunition + "]";
+	}
+
+	public Inventory() {
+		super();
+	}
+
+	public Inventory(long id, String location_id, String[] personal, String[] vehicles, String[] weapons,
+			int ammunition) {
+		super();
+		this.id = id;
+		this.location_id = location_id;
+		this.personal = personal;
+		this.vehicles = vehicles;
+		this.weapons = weapons;
+		this.ammunition = ammunition;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLocation_id() {
+		return location_id;
+	}
+
+	public void setLocation_id(String location_id) {
+		this.location_id = location_id;
+	}
+
+	public String[] getPersonal() {
+		return personal;
+	}
+
+	public void setPersonal(String[] personal) {
+		this.personal = personal;
+	}
+
+	public String[] getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(String[] vehicles) {
+		this.vehicles = vehicles;
+	}
+
+	public String[] getWeapons() {
+		return weapons;
+	}
+
+	public void setWeapons(String[] weapons) {
+		this.weapons = weapons;
+	}
+
+	public int getAmmunition() {
+		return ammunition;
+	}
+
+	public void setAmmunition(int ammunition) {
+		this.ammunition = ammunition;
+	}
+
+}
