3cbff3c03ae7eff1596adca4cd5f416ac247a8f5
Jacob Dean <JacobDean117@gmail.com>
2019-03-21 10:28:57 -0500

    added search feature to itemrepo

--- maven-project/warstars/src/main/java/com/team4/warstars/ItemRepo.java
+++ maven-project/warstars/src/main/java/com/team4/warstars/ItemRepo.java
@@ -1,6 +1,9 @@
 package com.team4.warstars;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -8,5 +11,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel="items", path="items")
 public interface ItemRepo extends CrudRepository<Item, Long>
 {
-
+	List<Item> findByCategory(@Param("category")String r); //url search?key=value
 }
