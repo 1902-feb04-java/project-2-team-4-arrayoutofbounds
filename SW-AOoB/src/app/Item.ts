45288355b3b04d859ec1ebd68cf78d870ac262f0
Brandon Lee <tubocass@gmail.com>
2019-03-22 12:10:19 -0500

    fixed the order stringification process, so that the db gets the right data

--- SW-AOoB/src/app/Item.ts
+++ SW-AOoB/src/app/Item.ts
@@ -4,4 +4,13 @@ export class Item{
     classification:string;
     model:string;
     cost:number;
+
+    constructor(obj)
+    {
+        this.id = obj.id;
+        this.category = obj.category;
+        this.classification = obj.classification;
+        this.model = obj.model;
+        this.cost = obj.cost;
+    }
 }
