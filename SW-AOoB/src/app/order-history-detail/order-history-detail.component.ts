104db30b043fe9799abddb7d361ade7b1dfad61e
gamalieljrz@gmail.com <gamalieljrz@gmail.com>
2019-03-23 14:49:40 -0500

    Working on displaying item details...info is being received but unable to display each property of array received.

--- SW-AOoB/src/app/order-history-detail/order-history-detail.component.ts
+++ SW-AOoB/src/app/order-history-detail/order-history-detail.component.ts
@@ -3,11 +3,15 @@ import { Order } from '../models/Order';
 import { OrderService } from '../services/order.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { Item } from '../models/Item'
-import { map } from 'rxjs/operators'
+import { Item } from '../models/Item';
+import { map, subscribeOn } from 'rxjs/operators';
+import { textBinding } from '@angular/core/src/render3';
 
-let ORDER_DETAILS= [];
-let ITEM_DETAILS: Item[] = [];
+let ORDER_DETAILS = [];
+
+let displayCost = document.getElementById('cost-id');
+let displayAuthorization = document.getElementById('auth-id');
+
 
 @Component({
   selector: 'app-order-history-detail',
@@ -15,6 +19,7 @@ let ITEM_DETAILS: Item[] = [];
   styleUrls: ['./order-history-detail.component.css']
 })
 export class OrderHistoryDetailComponent implements OnInit  {
+ ITEM_DETAILS: Item[] = null;
  @Input() order:Order; 
 
   constructor(
@@ -29,29 +34,33 @@ export class OrderHistoryDetailComponent implements OnInit  {
     //   this.parseData();
     //   console.log(ORDER_DETAILS);
     //  })
-    this.getOrderById();
-  }
-
-  getOrderById(){
     const orderId = +this.route.snapshot.paramMap.get('id');
-      console.log(orderId);
-      this.orderService.getOrderById(orderId)
-        .subscribe((order) => {
-          ITEM_DETAILS = order.itemsOrdered
-          this.parseData()
-         
+    this.orderService.getOrderById(orderId).subscribe((items) =>{
+        this.ITEM_DETAILS = items.itemsOrdered;
+        this.parseData;
+        console.log(this.ITEM_DETAILS)
+    })
 
-          ORDER_DETAILS[0] =order.orderId// <Order> (<unknown> order.orderId);
-          ORDER_DETAILS[1] = order.cost //<Order> (<unknown> order.cost);
-          ORDER_DETAILS[2] = order.authorizationRequired;//<Order> (<unknown> order.isAuthorized);
-          ORDER_DETAILS[3] = order.itemsOrdered//<Order> (<unknown> order.itemsOrdered);
-        })
-        console.log("order details:");
-        console.log(ORDER_DETAILS);
-        console.log("\nItems details");
-        console.log(ITEM_DETAILS);
-    
   }
+
+  // getOrderById(){
+  //   const orderId = +this.route.snapshot.paramMap.get('id');
+  //     console.log(orderId);
+  //     this.orderService.getOrderById(orderId)
+  //       .subscribe((order) => {
+  //         ITEM_DETAILS = JSON.parse(order.itemsOrdered.toString());
+  //         this.parseData();
+    
+  //        // let displayOrderId = order.orderId// <Order> (<unknown> order.orderId);
+  //         ORDER_DETAILS[1] = order.cost //<Order> (<unknown> order.cost);
+  //         ORDER_DETAILS[2] = order.authorizationRequired;//<Order> (<unknown> order.isAuthorized);
+  //         ORDER_DETAILS[3] = order.itemsOrdered//<Order> (<unknown> order.itemsOrdered);
+  //       })
+  //       console.log("order details");
+  //       console.log(ORDER_DETAILS);
+  //       console.log("\nItems details");
+    
+  // }
 
   //Back Button
   goBack(): void{
@@ -59,13 +68,12 @@ export class OrderHistoryDetailComponent implements OnInit  {
   }
 
   parseData(){
-    //<Item []> 
-    // let items;
-    //   items = JSON.parse(ITEM_DETAILS as unknown as string);
-      // items.array.forEach(element => {
-      //   ITEM_DETAILS.push(new Item(element))
-      // });
-      //ITEM_DETAILS = items;
+    let items;
+    this.ITEM_DETAILS.forEach(od =>{
+      //when using parse Error -> 'Unexpected token w in JSON in position 0
+      items = JSON.stringify(this.ITEM_DETAILS as unknown as string)
+      this.ITEM_DETAILS = items;
+    })
   }
 
 }
