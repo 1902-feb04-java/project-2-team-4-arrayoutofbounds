04d95c5e86437442cbcbdf94549a9715ba828b8c
gamalieljrz@gmail.com <gamalieljrz@gmail.com>
2019-03-23 22:39:01 -0500

    Added items length to be displayed in order history

--- SW-AOoB/src/app/OrderHistory/OrderHIstory.component.ts
+++ SW-AOoB/src/app/OrderHistory/OrderHIstory.component.ts
@@ -62,7 +62,7 @@ export class OrderHistoryComponent {
   ngOnInit() {
     this.orderService.getOrders().subscribe((orders) =>{
       ORDER_DATA = orders._embedded.orders;
-      //this.parseData();
+      this.parseData();
       console.log(ORDER_DATA)
       this.dataSource = new MatTableDataSource<Order>(ORDER_DATA);
     })
--- SW-AOoB/src/app/services/item.service.ts
+++ SW-AOoB/src/app/services/item.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Item} from '../models/Item'
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
+
 
   constructor(private http: HttpClient) { }
   private itemsURL = 'http://swirl-env.4jnneajyag.us-east-2.elasticbeanstalk.com/items';
@@ -35,4 +38,5 @@ export class ItemService {
   addItem(item:Item): Observable<Item>{
     return this.http.post<any>(this.itemsURL, item, this.httpOptions);
   }
+
 }
