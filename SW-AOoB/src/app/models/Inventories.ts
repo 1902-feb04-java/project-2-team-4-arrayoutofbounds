67ac67a2be830fd2954caf3ac98772f64b43d0ba
gamalieljrz@gmail.com <gamalieljrz@gmail.com>
2019-03-25 01:49:20 -0500

    Fixed order detail font, inventory font. Information can now be seen

--- SW-AOoB/src/app/models/Inventories.ts
+++ SW-AOoB/src/app/models/Inventories.ts
@@ -5,11 +5,8 @@ export class Inventories{
     items:Map<number,number>
 
     constructor(obj){
-<<<<<<< HEAD
         this.id = obj.id;
-=======
         this.id =  obj.inventoryid;
->>>>>>> 0d005618429ac570128aa020c24ced6de193e3ed
         this.locationId = obj.locationId;
         this.items = new Map<number, number>();
     }
