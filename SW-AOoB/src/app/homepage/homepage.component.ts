bcb69ad86b856786c3bf80f2b435177e3e1399bb
Rodolfo <rudymartinez70@gmail.com>
2019-03-24 22:26:13 -0500

    small changes and high-inventory start

--- SW-AOoB/src/app/homepage/homepage.component.ts
+++ SW-AOoB/src/app/homepage/homepage.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Officer } from '../models/Officer';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
+  officer: Officer
   constructor() { }
   ngOnInit() {
+    this.officer = JSON.parse(localStorage.getItem('officer'))
+    console.log(this.officer)
   }
 }
